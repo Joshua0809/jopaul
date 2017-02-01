2f485a0c322d118c2401c8d83e4afc7a9776fe95
Joshua0809 <joshuasampaul@gmail.com>
2017-02-01 17:15:48 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+        default = "eu-west-1"
+}
+variable "availability_zone" {
+        default = "eu-west-1c"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-b9b394ca"
+}
+
+variable "key_name" {
+        default = "Josam001" }
+
+variable "aws_access_key" {
+        default = "AKIAIHLUQPPUBEHYVYA2mMJ5mjsBrjYctZUyWIuSSTn3VGHKUOL5AL4/xwZA"
+}
+
+variable "aws_secret_key" {
+        default = "2mMJ5mjsBrjYctZUyWIuSSTn3VGHKUOL5AL4/xwZ"
+}
+
